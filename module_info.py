7d36d4f810672d43d5822ca56c33868144f09641
Steven Schwartfeger <vornne@gmail.com>
2010-04-21 02:09:12 +1200

    Set output path.

--- module_info.py
+++ module_info.py
@@ -2,5 +2,4 @@
 # Point export_dir to the folder you will be keeping your module
 # Make sure you use forward slashes (/) and NOT backward slashes (\)
 
-export_dir = "../WOTS/Modules/Native/"
-#export_dir = "C:/Program Files/Mount&Blade/Modules/Native/"
+export_dir = "/home/steven/.wine/drive_c/Program Files/Mount&Blade Warband/Modules/Native/"
