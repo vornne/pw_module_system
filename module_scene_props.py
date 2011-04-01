bbfe700e3d44ab48814d21c269268ba911d10b55
Steven Schwartfeger <vornne@gmail.com>
2011-04-01 22:05:57 +1300

    Adjust cart attach point and detach animation.

--- module_scene_props.py
+++ module_scene_props.py
@@ -211,10 +211,11 @@ def spr_portcullis_winch_triggers(target_scene_prop, move_steps=5, step_size=100
     [link_scene_prop, target_scene_prop],
     [init_scene_prop, "script_cf_init_winch", move_steps, step_size, winch_type_portcullis]]
 
-def spr_cart_triggers(horse=0, z_offset=0, inventory_count=0, max_item_length=100):
+def spr_cart_triggers(horse=0, detach_offset=0, detach_rotation=0, inventory_count=0, max_item_length=100):
   return [(ti_on_scene_prop_init,
      [(store_trigger_param_1, ":instance_id"),
-      (scene_prop_set_slot, ":instance_id", slot_scene_prop_position, z_offset),
+      (scene_prop_set_slot, ":instance_id", slot_scene_prop_position, detach_offset),
+      (scene_prop_set_slot, ":instance_id", slot_scene_prop_rotation, detach_rotation),
       (scene_prop_set_slot, ":instance_id", slot_scene_prop_attached_to_agent, -1),
       (scene_prop_set_slot, ":instance_id", slot_scene_prop_inventory_count, inventory_count),
       (scene_prop_set_slot, ":instance_id", slot_scene_prop_length, max_item_length),
@@ -2358,10 +2359,10 @@ scene_props = [
   ("pw_drawbridge_a",sokf_moveable,"drawbridge","bo_drawbridge", []),
   ("pw_drawbridge_winch_b",sokf_moveable|spr_use_time(2),"winch_b","bo_winch", spr_drawbridge_winch_triggers("pw_drawbridge_b")),
   ("pw_drawbridge_b",sokf_moveable,"castle_drawbridges_open","bo_castle_drawbridges_open", []),
-  ("pw_cart_a",sokf_moveable|spr_use_time(1),"pw_cart_a","bo_pw_cart_a", spr_cart_triggers(horse=1, z_offset=-125, inventory_count=51, max_item_length=200)),
-  ("pw_cart_b",sokf_moveable|spr_use_time(1),"pw_cart_b","bo_pw_cart_b", spr_cart_triggers(horse=1, z_offset=-125, inventory_count=32, max_item_length=150)),
-  ("pw_wheelbarrow",sokf_moveable|spr_use_time(1),"pw_hand_cart_a","bo_pw_hand_cart_a", spr_cart_triggers(z_offset=-90, inventory_count=3, max_item_length=70)),
-  ("pw_hand_cart",sokf_moveable|spr_use_time(1),"pw_hand_cart_b","bo_pw_hand_cart_b", spr_cart_triggers(z_offset=-90, inventory_count=5, max_item_length=100)),
+  ("pw_cart_a",sokf_moveable|spr_use_time(1),"pw_cart_a","bo_pw_cart_a", spr_cart_triggers(horse=1, detach_offset=60, detach_rotation=-20, inventory_count=51, max_item_length=250)),
+  ("pw_cart_b",sokf_moveable|spr_use_time(1),"pw_cart_b","bo_pw_cart_b", spr_cart_triggers(horse=1, detach_offset=110, detach_rotation=-6, inventory_count=32, max_item_length=250)),
+  ("pw_wheelbarrow",sokf_moveable|spr_use_time(1),"pw_hand_cart_a","bo_pw_hand_cart_a", spr_cart_triggers(detach_offset=47, detach_rotation=15, inventory_count=3, max_item_length=120)),
+  ("pw_hand_cart",sokf_moveable|spr_use_time(1),"pw_hand_cart_b","bo_pw_hand_cart_b", spr_cart_triggers(detach_offset=90, inventory_count=5, max_item_length=150)),
 
   ("pw_ship_a",sokf_moveable|sokf_destructible|sokf_show_hit_point_bar,"pw_ship_a","bo_pw_ship_a", spr_ship_triggers(hit_points=7000, length=800, width=150, speed=6, sail="pw_ship_a_sail", sail_off="pw_ship_a_sail_off", collision="pw_ship_a_cd")),
   ("pw_ship_a_sail",sokf_moveable,"pw_ship_a_sail","bo_pw_ship_a_sail", []),
