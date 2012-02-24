dd668b80ac247631d28b8417e31e0cc056435fc0
Steven Schwartfeger <vornne@gmail.com>
2012-02-25 12:13:36 +1300

    Clean up script generation.

--- module_generated_scripts.py
+++ module_generated_scripts.py
@@ -2,19 +2,14 @@
 # This file is for scripts that are generated automatically from lists generated in other module files,
 # which needs to be executed once just before writing scripts.txt, rather than every time module_scripts is loaded,
 # to avoid extra or cyclic dependencies.
+#
+# To use, make a function called "generate_" then the script name which returns the script body.
 ####################################################################################################################
 
-from module_scripts import *
+from header_skills import *
+from header_troops import *
 from module_items import *
 from module_scene_props import *
-
-def generate_script(name, body):
-  for script in scripts:
-    if script[0] == name:
-      script[1].extend(body)
-      return
-  else:
-    raise Exception("Script '" + name + "' not found")
 
 def generate_store_troop_skills_description():
   script_body = [(store_script_param, ":troop_id", 1)]
@@ -27,7 +22,6 @@ def generate_store_troop_skills_description():
       (try_end),
       ])
   return script_body
-generate_script("store_troop_skills_description", generate_store_troop_skills_description())
 
 def generate_initialize_item_slots():
   script_body = []
@@ -49,14 +43,12 @@ def generate_initialize_item_slots():
     if len(entry) > 2 and entry[2] > 0:
       script_body.append((item_set_slot, entry[0], slot_item_resource_amount, entry[2]))
   return script_body
-generate_script("initialize_item_slots", generate_initialize_item_slots())
 
 def generate_setup_all_linked_scene_props():
   script_body = []
   for link_entry in scene_props_to_link:
     script_body.append(tuple([call_script, "script_setup_linked_scene_props"] + link_entry))
   return script_body
-generate_script("setup_all_linked_scene_props", generate_setup_all_linked_scene_props())
 
 def generate_setup_scene_props_after_mission_start():
   script_body = []
@@ -69,4 +61,3 @@ def generate_setup_scene_props_after_mission_start():
       (try_end),
       ])
   return script_body
-generate_script("setup_scene_props_after_mission_start", generate_setup_scene_props_after_mission_start())
--- process_scripts.py
+++ process_scripts.py
@@ -1,6 +1,18 @@
+import process_common as pc
 import process_operations as po
 import module_scripts
 import module_generated_scripts
+
+def generate_scripts(processor, unused):
+  for script_name, script_generator in module_generated_scripts.__dict__.iteritems():
+    if script_name.startswith("generate_"):
+      try:
+        script_id = processor.process_id(script_name.replace("generate_", "script_"), "script")
+        module_scripts.scripts[script_id][1].extend(script_generator())
+      except pc.ModuleSystemError as e:
+        if not e.entry:
+          e.entry = "generated scripts"
+        raise
 
 def process_entry(processor, txt_file, entry, index):
   output_list = ["%s -1\r\n" % entry[0]]
@@ -9,4 +21,4 @@ def process_entry(processor, txt_file, entry, index):
   txt_file.write("".join(output_list))
 
 export = po.make_export(data=module_scripts.scripts, data_name="scripts", tag="script",
-    header_format="scriptsfile version 1\r\n%d\r\n", process_entry=process_entry)
+    header_format="scriptsfile version 1\r\n%d\r\n", process_entry=process_entry, process_list=generate_scripts)
