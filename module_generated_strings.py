7dce20ec0afb16f34e56dbc40d0280bd5a856de9
Steven Schwartfeger <vornne@gmail.com>
2012-04-06 15:15:13 +1200

    Enable multiple skills to be used for crafting, adding crafting methods for armor and horses.

--- module_generated_strings.py
+++ module_generated_strings.py
@@ -7,16 +7,51 @@
 from module_items import *
 from module_scene_props import *
 
-def generate_book_of_weapons(string, processor):
+def process_item(processor, item_str, entry=None):
+  try:
+    return items[processor.process_id(item_str)]
+  except Exception as e:
+    if not e.entry:
+      if entry:
+        e.entry = "crafting info for %s" % (entry)
+      else:
+        e.entry = "crafting info"
+    raise
+
+def crafting_book_generator(string, processor, item_type_checker):
   crafting_list = [string]
   for entry in crafting_info:
-    crafting_list.extend([items[processor.process_id(entry[0])][1], ": "])
+    item = process_item(processor, entry[0])
+    if not item_type_checker(item[3] & 0xff, entry[1][0][0]):
+      continue
+    crafting_list.extend([item[1], ": "])
     for i, resource in enumerate(entry[2]):
       if i > 0:
         crafting_list.append(", ")
       if isinstance(resource, tuple):
         crafting_list.extend([str(resource[1]), " "])
         resource = resource[0]
-      crafting_list.append(items[processor.process_id(resource)][1])
+      crafting_list.append(process_item(processor, resource, entry[0])[1])
     crafting_list.append("^^")
   return "".join(crafting_list)
+
+def type_is_armor(item_type):
+  return itp_type_head_armor <= item_type <= itp_type_hand_armor
+
+def is_weapon(item_type, skill):
+  return not type_is_armor(item_type)
+
+def generate_book_of_weapons(string, processor):
+  return crafting_book_generator(string, processor, is_weapon)
+
+def is_clothing(item_type, skill):
+  return type_is_armor(item_type) and skill == "skl_tailoring"
+
+def generate_book_of_clothing(string, processor):
+  return crafting_book_generator(string, processor, is_clothing)
+
+def is_armor(item_type, skill):
+  return type_is_armor(item_type) and skill == "skl_engineer"
+
+def generate_book_of_armor(string, processor):
+  return crafting_book_generator(string, processor, is_armor)
