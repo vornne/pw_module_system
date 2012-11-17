397d70298e97be829a5645d193be20d900aef1c9
Steven Schwartfeger <vornne@gmail.com>
2012-11-17 20:05:05 +1300

    Document the format of mission template triggers.

--- module_mission_templates.py
+++ module_mission_templates.py
@@ -12,25 +12,29 @@ import header_lazy_evaluation as lazy
 ####################################################################################################################
 #   Each mission-template is a tuple that contains the following fields:
 #  1) Mission-template id (string): used for referencing mission-templates in other files.
-#     The prefix mt_ is automatically added before each mission-template id
-#
+#     The prefix mt_ is automatically added before each mission-template id.
 #  2) Mission-template flags (int): See header_mission-templates.py for a list of available flags
-#  3) Mission-type(int): Which mission types this mission template matches.
+#  3) Mission-type (int): Which mission types this mission template matches.
 #     For mission-types to be used with the default party-meeting system,
 #     this should be 'charge' or 'charge_with_ally' otherwise must be -1.
-#
 #  4) Mission description text (string).
-#  5) List of spawn records (list): Each spawn record is a tuple that contains the following fields:
-#    5.1) entry-no: Troops spawned from this spawn record will use this entry
-#    5.2) spawn flags.
-#    5.3) alter flags. which equipment will be overriden
-#    5.4) ai flags.
-#    5.5) Number of troops to spawn.
-#    5.6) list of equipment to add to troops spawned from here (maximum 8).
-#  6) List of triggers (list).
-#     See module_triggers.py for infomation about triggers.
 #
-#  Please note that mission templates is work in progress and can be changed in the future versions.
+#  5) List of spawn records (list): Each spawn record is a tuple that contains the following fields:
+#    5.1) Entry point number: Troops spawned from this spawn record will use this entry.
+#    5.2) Spawn flags.
+#    5.3) Alter flags: which equipment will be overriden.
+#    5.4) AI flags.
+#    5.5) Number of troops to spawn.
+#    5.6) List of equipment to add to troops spawned from here (maximum 8).
+#
+#  6) List of triggers (list): Each trigger contains the following fields:
+#    6.1) Check interval: How frequently this trigger will be checked. Also used for special triggers listed in header_triggers.py.
+#    6.2) Delay interval: Time to wait before applying the consequences of the trigger after its conditions have been evaluated as true.
+#    6.3) Re-arm interval. How much time must pass after applying the consequences of the trigger for the trigger to become active again.
+#    You can put the constant ti_once here to make sure that the trigger never becomes active again after it fires once.
+#    6.4) Conditions block (list), must be a valid operation block. Every time the trigger is checked, this block will be executed.
+#    If the conditions block returns true or is empty, the consequences block will be executed.
+#    6.5) Consequences block (list), must be a valid operation block. Executed only if the conditions block succeeded.
 ####################################################################################################################
 
 spawn_points_0_99 = [
