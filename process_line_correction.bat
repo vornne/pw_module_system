d525d0d1db12da3a55e57e87c3d1015f6502a841
Steven Schwartfeger <vornne@gmail.com>
2013-02-18 00:48:26 +1300

    Remove old line correction script from native that does not use the PW style.

--- process_line_correction.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-python process_line_correction.py
-del *.pyc
-echo.
-echo ______________________________
-echo.
-echo Script processing has ended.
-echo Press any key to exit. . .
-pause>nul
