de2c39d92b32976af6a0664d043cf091b6f6c412
Steven Schwartfeger <vornne@gmail.com>
2013-08-24 15:07:09 +1200

    Requested banners.

--- module_meshes.py
+++ module_meshes.py
@@ -316,6 +316,8 @@ meshes = [
   ("banner_l03", 0, "pw_banner_l03", 0, 0, 0, -90, 0, 0, 1, 1, 1),
   ("banner_l04", 0, "pw_banner_l04", 0, 0, 0, -90, 0, 0, 1, 1, 1),
   ("banner_l05", 0, "pw_banner_l05", 0, 0, 0, -90, 0, 0, 1, 1, 1),
+  ("banner_l06", 0, "pw_banner_l06", 0, 0, 0, -90, 0, 0, 1, 1, 1),
+  ("banner_l07", 0, "pw_banner_l07", 0, 0, 0, -90, 0, 0, 1, 1, 1),
 
   ("banner_kingdom_a", 0, "pw_banner_kingdom_a", 0, 0, 0, -90, 0, 0, 1, 1, 1),
   ("banner_kingdom_b", 0, "pw_banner_kingdom_b", 0, 0, 0, -90, 0, 0, 1, 1, 1),
